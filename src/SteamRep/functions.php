d54b188222a118744f952acf68cdb6fc924b856d
fisk <git@fisk.cc>
2018-09-05 13:02:39 +0100

    remove JSON_INVALID_UTF8_SUBSTITUTE, constant added in 7.2

--- src/SteamRep/functions.php
+++ src/SteamRep/functions.php
@@ -4,6 +4,7 @@ namespace SteamRep;
 
 /**
  * A wrapper for json_decode that handles malformed JSON.
+ * JSON_INVALID_UTF8_SUBSTITUTE doesn't work to fix the UTF-16 surrogate issue and breaks <=7.1 support.
  *
  * @param $json
  * @param bool $assoc
@@ -12,12 +13,12 @@ namespace SteamRep;
  */
 function sr_json_decode($json, $assoc = false, $depth = 512)
 {
-    $out = json_decode($json, $assoc, $depth, JSON_INVALID_UTF8_SUBSTITUTE);
+    $out = json_decode($json, $assoc, $depth);
 
     if (json_last_error() === JSON_ERROR_NONE) {
         return $out;
     }
 
     $json = preg_replace('/\\\\u[0-9A-F]{4}/i', '', $json);
-    return json_decode($json, $assoc, $depth, JSON_INVALID_UTF8_SUBSTITUTE);
+    return json_decode($json, $assoc, $depth);
 }
